47ea13e97075c15870261c0e7e1a5b288f2a3f6a
gabrielvasile <gabriell.vasile@gmail.com>
2019-01-22 21:14:41 +0200

    Cache external resources

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,12 @@
 var CACHE_NAME = "pwa-cache";
 var ROOT = "/PWA/how-to-pwa/public";
 var urlsToCache = [
+  "https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css",
+  "https://fonts.googleapis.com/css?family=Lato|Open+Sans|PT+Serif|Roboto:300,400,500,700|Material+Icons|Ubuntu|Vollkorn",
+  "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js",
+  "https://cdn.rawgit.com/FezVrasta/snackbarjs/1.1.0/dist/snackbar.min.js",
+  "https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js",
   ROOT + "/",
   ROOT + "/css/style.css",
   ROOT + "/css/prism.css",
