f34ec898ee3494793e73f172628799b66ab88011
gabrielvasile <gabriell.vasile@gmail.com>
2019-01-22 14:35:36 +0200

    Caching & new theme

--- public/caching/index.php
+++ public/caching/index.php
@@ -3,6 +3,152 @@
     <div class="row">
     <div class="col">
         <h2>4. Caching</h2>
+
+        <p>It doesn't matter how much caching you do, the ServiceWorker won't use the cache unless you tell it when & how. Here are a few patterns for handling requests:</p>
+
+        <h4>Cache only</h4>
+        <p>Ideal for: Anything you'd consider static to that "version" of your site. You should have cached these in the install event, so you can depend on them being there.</p>
+
+        <pre><code class="language-javascript">
+self.addEventListener('fetch', (event) => {
+  // If a match isn't found in the cache, the response
+  // will look like a connection error
+  event.respondWith(caches.match(event.request));
+});        
+        </code></pre>
+
+        <h4>Network only</h4>
+        <p>Ideal for: Things that have no offline equivalent, such as analytics pings, non-GET requests.</p>
+
+        <pre><code class="language-javascript">
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+  // or simply don't call event.respondWith, which
+  // will result in default browser behaviour
+});      
+        </code></pre>
+
+        <h4>Cache, falling back to network</h4>
+        <p>Ideal for: If you're building offline-first, this is how you'll handle the majority of requests. Other patterns will be exceptions based on the incoming request.</p>
+
+        <pre><code class="language-javascript">
+self.addEventListener('fetch', (event) => {
+  event.respondWith(async function() {
+    const response = await caches.match(event.request);
+    return response || fetch(event.request);
+  }());
+});
+        </code></pre>
+
+        <h4>Cache & network race</h4>
+        <p>Ideal for: Small assets where you're chasing performance on devices with slow disk access.</p>
+
+        <pre><code class="language-javascript">
+// Promise.race is no good to us because it rejects if
+// a promise rejects before fulfilling. Let's make a proper
+// race function:
+function promiseAny(promises) {
+  return new Promise((resolve, reject) => {
+    // make sure promises are all promises
+    promises = promises.map(p => Promise.resolve(p));
+    // resolve this promise as soon as one resolves
+    promises.forEach(p => p.then(resolve));
+    // reject if all promises reject
+    promises.reduce((a, b) => a.catch(() => b))
+      .catch(() => reject(Error("All failed")));
+  });
+};
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    promiseAny([
+      caches.match(event.request),
+      fetch(event.request)
+    ])
+  );
+});
+        </code></pre>
+
+        <h4>Network falling back to cache</h4>
+        <p>Ideal for: A quick-fix for resources that update frequently, outside of the "version" of the site. E.g. articles, avatars, social media timelines, game leader boards.</p>
+
+        <p>
+            <div class="alert alert-danger" role="alert">
+            <i class="material-icons"> warning </i>
+            If the user has an intermitent or slow connection they'll have to wait for the network to fail before they get the perfectly acceptable content already on their device. This can take an extremely long time and is a frustrating user experience. See the next pattern, "Cache then network", for a better solution.
+            </div>  
+        </p>
+
+        <pre><code class="language-javascript">
+self.addEventListener('fetch', (event) => {
+  event.respondWith(async function() {
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
+    }
+  }());
+});
+        </code></pre>
+        
+        <h4>Cache then network</h4>
+        <p>Ideal for: Content that updates frequently. E.g. articles, social media timelines, game leaderboards. This requires the page to make two requests, one to the cache, one to the network. The idea is to show the cached data first, then update the page when/if the network data arrives.</p>
+
+        <pre><code class="language-javascript">
+async function update() {
+  // Start the network request as soon as possible.
+  const networkPromise = fetch('/data.json');
+
+  startSpinner();
+
+  const cachedResponse = await caches.match('/data.json');
+  if (cachedResponse) await displayUpdate(cachedResponse);
+
+  try {
+    const networkResponse = await networkPromise;
+    const cache = await caches.open('mysite-dynamic');
+    cache.put('/data.json', networkResponse.clone());
+    await displayUpdate(networkResponse);
+  } catch (err) {
+    // Maybe report a lack of connectivity to the user.
+  }
+
+  stopSpinner();
+
+  const networkResponse = await networkPromise;
+
+}
+
+async function displayUpdate(response) {
+  const data = await response.json();
+  updatePage(data);
+}
+        </code></pre>
+        
+        <h4>Generic fallback</h4>
+        <p>If you fail to serve something from the cache and/or network you may want to provide a generic fallback. Ideal for: Secondary imagery such as avatars, failed POST requests, "Unavailable while offline" page.</p>
+
+        <pre><code class="language-javascript">
+self.addEventListener('fetch', (event) => {
+  event.respondWith(async function() {
+    // Try the cache
+    const cachedResponse = await caches.match(event.request);
+    if (cachedResponse) return cachedResponse;
+
+    try {
+      // Fall back to network
+      return await fetch(event.request);
+    } catch (err) {
+      // If both fail, show a generic fallback:
+      return caches.match('/offline.html');
+      // However, in reality you'd have many different
+      // fallbacks, depending on URL & headers.
+      // Eg, a fallback silhouette image for avatars.
+    }
+  }());
+});
+
+        </code></pre>
         
         <nav aria-label="Page navigation example">
             <ul class="pagination">
--- public/web-app-manifest/index.php
+++ public/web-app-manifest/index.php
@@ -22,41 +22,48 @@
             </code></pre>
         <h4>Example manifest</h4>    
         <pre><code class="language-javascript">{
-"name": "HackerWeb", // App name
-"short_name": "HackerWeb", // App short name
-"start_url": ".", // The URL that loads when a user launches the application
-"display": "standalone",
-"background_color": "#fff",
-"description": "A simply readable Hacker News app.",
-"icons": [{
-"src": "images/touch/homescreen48.png",
-"sizes": "48x48",
-"type": "image/png"
-}, {
-"src": "images/touch/homescreen72.png",
-"sizes": "72x72",
-"type": "image/png"
-}, {
-"src": "images/touch/homescreen96.png",
-"sizes": "96x96",
-"type": "image/png"
-}, {
-"src": "images/touch/homescreen144.png",
-"sizes": "144x144",
-"type": "image/png"
-}, {
-"src": "images/touch/homescreen168.png",
-"sizes": "168x168",
-"type": "image/png"
-}, {
-"src": "images/touch/homescreen192.png",
-"sizes": "192x192",
-"type": "image/png"
-}],
-"related_applications": [{
-"platform": "play",
-"url": "https://play.google.com/store/apps/details?id=cheeaun.hackerweb"
-}]
+    "name": "HackerWeb", // App name
+    "short_name": "HackerWeb", // App short name
+    "start_url": ".", // The URL that loads when a user launches the application
+    "display": "standalone",
+    "background_color": "#fff",
+    "description": "A simply readable Hacker News app.",
+    "icons": [
+        {
+            "src": "images/touch/homescreen48.png",
+            "sizes": "48x48",
+            "type": "image/png"
+        }, 
+        {
+            "src": "images/touch/homescreen72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+        }, 
+        {
+            "src": "images/touch/homescreen96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+        }, 
+        {
+            "src": "images/touch/homescreen144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+        }, 
+        {
+            "src": "images/touch/homescreen168.png",
+            "sizes": "168x168",
+            "type": "image/png"
+        }, 
+        {
+            "src": "images/touch/homescreen192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        }
+    ],
+    "related_applications": [{
+        "platform": "play",
+        "url": "https://play.google.com/store/apps/details?id=cheeaun.hackerweb"
+    }]
 }</code></pre>
         <h4>Members</h4>
 
