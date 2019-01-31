8e0c43eace0c62efdef4824c6096bb027a6b8a7b
gabrielvasile <gabriell.vasile@gmail.com>
2019-01-31 21:32:31 +0200

    Background sync

--- public/background-sync/index.php
+++ public/background-sync/index.php
@@ -2,55 +2,54 @@
 <div class="container">
     <div class="row">
     <div class="col">
-        <h2>6. Background Sync</h2>
+        <h2>6. Sincronizare pe fundal (Background Sync)</h2>
 
-        <p>Background sync is a new web API that lets you defer actions until the user has stable connectivity. This is useful for ensuring that whatever the user wants to send, is actually sent. The user can go offline and even close the browser, safe in the knowledge that their request will be sent when they regain connectivity.</p>
-        <p>Service Workers provides offline functionality and dramatically speeds up load times using caching. While this is useful, there has been no way for the page to actually send something to the server without connectivity. Background Sync is the feature that has been built to handle just such a scenario.</p>
+        <p>Background Sync este un API nou ce permite amanarea actiunilor pana cand utilizatorul are acces stabil la internet. Aceasta functionalitate ajuta aplicatia sa functioneze cursiv, iar ceea ce doreste sa posteze utilizatorul chiar sa ajunga pe server. Utilizatorul poate inchide accesul la internet si chiar browserul, iar ceea ce a dorit sa posteze se va trimite catre server cand dispozitivul va avea din nou acces la internet.</p>
+        <p>Fisierele Service Worker ofera functionalitate offline creste dramatic viteza de incarcare a aplicatiei folosind cache-ul. Chiar daca aceasta functionalitate este foarte utila, pana la API-ul Background Sync nu a existat o posibilitate ca pagina sa trimita ceva catre server fara conexiune la internet, insa noul API Background Sync tocmai pentru aceasta a fost creeat.</p>
 
-        <p>Browser support for Background Sync API can be checked here: <a href="https://caniuse.com/#search=background%20sync%20api" target="_blank">Browser Sync API browser support</a></p>
+        <p>Suportul browserelor pentu API-ul Background Sync poate fi verificat aici: <a href="https://caniuse.com/#search=background%20sync%20api" target="_blank">Suport browsere Background Sync</a></p>
 
         <figure class="figure">
           <a href="https://caniuse.com/#search=background%20sync%20api" target="_blank">
-            <img src="<?php echo ROOT ?>img/background-sync-api.png" class="figure-img img-fluid rounded" alt="Browser Sync API browser support">
+            <img src="<?php echo ROOT ?>img/background-sync-api.png" class="figure-img img-fluid rounded" alt="Suport browsere Background Sync">
           </a>
-          <figcaption class="figure-caption">Browser Sync API browser support</figcaption>
+          <figcaption class="figure-caption">Suport browsere Background Sync</figcaption>
         </figure>
 
-        <p>If you write an email, instant message, or simply favorite a tweet, the application needs to communicate that data to the server. If that fails, either due to user connectivity, service availability or anything in-between, the app can store that action in some kind of 'outbox' for retry later.</p>
+        <p>Cand scrii un email, un mesaj pe Facebook, WhatsApp, Twitter, etc., aplicatia are nevoie sa trimita mesajul catre server. Daca nu poate face asta din cauza lipsei conexiunii la internet, aplicatia poate salva mesajul intr-o sectiune numita "outbox" pentru a incerca mai tarziu.</p>
 
         <p>
             <div class="alert alert-info" role="alert">
             <i class="material-icons"> info </i>
-            Unfortunately, on the web, that outbox can only be processed while the site is displayed in a browsing context. This is particularly problematic on mobile, where browsing contexts are frequently shut down to free memory.
+            Din nefericire, in browser sectiuna "outbox" poate fi procesata doar in timp ce website-ul este afisat in contextul unui browser. Aceasta situatie este problematica in mod special pe dispozitivele mobile, unde utilizatorii inchid deseori browserele pentru a elibera memoria dispozitivului.
             </div>  
         </p>
 
-        <p>Native application platforms provide job scheduling APIs that enable developers to collaborate with the system to ensure low power usage and background-driven processing. The web platform needs capabilities like this too. That's where Background Sync comes into play.</p>
+        <p>Platformele aplicatiilor mobile native ofera API-uri care pot programa sarcini. Aceste API-uri permit programatorilor sa colaboreze cu sistemul de operare pentru procesari in fundal. API-ul Background Sync a fost creat pentru a oferi calitati similare pentru website-uri.</p>
 
-        <h4>What could I use Background Sync for?</h4>
+        <h4>Pentru ce putem folosi API-ul Background Sync?</h4>
 
-        <p>Ideally, you’d use it to schedule any data sending that you care about beyond the life of the page. Chat messages, emails, document updates, settings changes, photo uploads… anything that you want to reach the server even if user navigates away or closes the tab. The page could store these in an "outbox" store in indexedDB, and the service worker would retrieve them, and send them.</p>
-        <p>If you write an email, instant message, or simply favorite a tweet, the application needs to communicate that data to the server. If that fails, either due to user connectivity, service availability or anything in-between, the app can store that action in some kind of 'outbox' for retry later.</p>
+        <p>In mod ideal, il putem folosi pentru a programa trimiterea datelor care nu dorim sa se piarda atunci cand dispozitivul folosit nu mai are acces la internet ca de exemplu mesaje, email-uri, documente incarcate, setari salvate, poze... orice dorim sa ne asiguram ca ajunge pe server chiar daca utilizatorul inchide aplicatia sau nu mai are acces la internet. Aplicatia le poate salva folosind indexedDB, iar fisierul Service Worker le va accesa si le va trimite catre server.</p>
 
         <p>
             <div class="alert alert-danger" role="alert">
             <i class="material-icons"> warning </i>
-            Sync may not be available to all web applications, not even all apps served over SSL. Browsers may choose to limit the set of applications which can register for synchronization based on quality signals that aren't a part of the visible API. This is especially true of periodic sync.
+            API-url Background Sync nu este disponibil pentru toate aplicatiile, chiar daca acestea folosesc SSL. Browserele pot alege sa limiteze aplicatiiile care pot folosi aceasta functionalitate in functie de anumite semnale legate de calitatea aplicatiei.
             </div>  
         </p>
     
-        <h4>Requesting a sync</h4>
+        <h4>Solicitarea unei sincronizari</h4>
         <pre><code class="language-javascript">
 navigator.serviceWorker.ready.then(function(registration) {
   registration.sync.register('outbox').then(function() {
-    // registration succeeded
+    // Inregistrarea a fost facuta cu succes
   }, function() {
-    // registration failed
+    // Inregistrarea a esuat
   });
 });
         </code></pre>
 
-        <h4>Responding to a sync</h4>
+        <h4>Interceptarea unei cereri de sincronizare</h4>
         <pre><code class="language-javascript">
 self.addEventListener('sync', function(event) {
   if (event.tag == 'outbox') {
@@ -59,43 +58,43 @@ self.addEventListener('sync', function(event) {
 });
         </code></pre>
 
-        <p>sync will fire when the user agent believes the user has connectivity.</p>
+        <p>Evenimentul "sync" va fi declansat in momentul in care browserul considera ca dispozitivul are acces la internet.</p>
 
-        <h4>Periodic synchronization (in design)</h4>
+        <h4>Sincronizare periodica (in lucru)</h4>
 
-        <p>Opening a news or social media app to find content you hadn't seen before - without going to the network, is a user experience currently limited to native apps.</p>
-        <p>An example of using periodic syncing is if the user agent knows the user has a morning alarm set, it may run synchronizations shortly beforehand, giving the user quick and up-to-date information from their favorite sites.</p>
+        <p>Unele aplicatii mobile native de stiri sau social media (ex. Twitter) ofera continut actualizat fara ca utilizatorul sa ceara explicit.</p>
+        <p>Folosind sincronizarea periodica, vor putea si website-uri sa ofere aceasta functionalitate. De exemplu daca telefonul mobil are setata o alarma, fisierul Service Worker poate solicita prin API-ul Background Sync actualizarea aplicatiei PWA la un minut inainte, pentru a oferi utilizatorului informatii actualizate de la website-ul preferat.</p>
 
-        <h4>To request a periodic sync</h4>
+        <h4>Solicitarea unei sincronizari periodice</h4>
         <pre><code class="language-javascript">
 navigator.serviceWorker.ready.then(function(registration) {
   registration.periodicSync.register({
-    tag: 'get-latest-news',         // default: ''
-    minPeriod: 12 * 60 * 60 * 1000, // default: 0
-    powerState: 'avoid-draining',   // default: 'auto'
-    networkState: 'avoid-cellular'  // default: 'online'
+    tag: 'get-latest-news',         
+    minPeriod: 12 * 60 * 60 * 1000, 
+    powerState: 'avoid-draining',   
+    networkState: 'avoid-cellular' 
   }).then(function(periodicSyncReg) {
-    // success
+    // Succes
   }, function() {
-    // failure
+    // Eroare
   })
 });
         </code></pre>
 
-        <h4>To respond to a periodic sync</h4>
+        <h4>Interceptarea unei cereri de sincronizare periodica</h4>
         <pre><code class="language-javascript">
 self.addEventListener('periodicsync', function(event) {
   if (event.registration.tag == 'get-latest-news') {
     event.waitUntil(fetchAndCacheLatestNews());
   }
   else {
-    // unknown sync, may be old, best to unregister
+    // asa putem sterge cererea de sincronizare
     event.registration.unregister();
   }
 });
         </code></pre>
 
-        <p>The promise passed to waitUntil is a signal to the user agent that the sync event is ongoing and that it should keep the service worker alive if possible. Rejection of the event signals to the user agent that the sync failed.</p>
+        <p>Functia de tip promisiune pasata functiei waitUntil reprezinta un semnal pentru browser ca evenimentul de sincronizare este in lucru si cere ca fisierul Service Worker sa fie activ. Respingerea evenimentului semnaleaza browserului ca evenimentul de sincronizare a esuat.</p>
         
         <nav aria-label="Page navigation example">
             <ul class="pagination">
@@ -107,7 +106,7 @@ self.addEventListener('periodicsync', function(event) {
                 </li>
                 <li class="page-item forward">
                     <a class="page-link" href="<?php echo ROOT ?>push-notifications/" aria-label="Next">
-                        <span class="paginationDesc">Push Notifications</span>
+                        <span class="paginationDesc">Notificari Push</span>
                         <span aria-hidden="true">&raquo;</span>
                     </a>
                 </li>
