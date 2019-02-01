ce4585f66945613a59e22d0ace4609c31d6a5e89
gabrielvasile <gabriell.vasile@gmail.com>
2019-02-01 15:20:04 +0200

    Functionalitati native

--- public/native-device-features/index.php
+++ public/native-device-features/index.php
@@ -2,67 +2,67 @@
 <div class="container">
     <div class="row">
     <div class="col">
-        <h2>8. Native Device Features</h2>
+        <h2>8. Functionalitati native dispozitivelor mobile</h2>
 
-        <h3>a) The Stream API</h3>
+        <h3>a) API-ul Stream</h3>
 
-        <p>The Stream API getUserMedia() method prompts the user for permission to use a media input which produces a MediaStream with tracks containing the requested types of media.</p>
-        <p>That stream can include a video track (produced by a camera, video recording device, screen sharing service), an audio track (similarly, produced a microphone), and possibly other track types.</p>
-
-        <p>Browser support for Stream API can be checked here: <a href="https://caniuse.com/#search=stream%20api" target="_blank">Stream API browser support</a></p>
+        <p>Functia getUserMedia() din API-ul Stream cere utilzatorului accesul la camera foto/video si la microfonul dispozitivului mobil.</p>
+        <p>Suportul browserelor pentru API-ul Stream poate fi verificat aici: <a href="https://caniuse.com/#search=stream%20api" target="_blank">suportul pentru Stream API</a></p>
 
         <figure class="figure">
           <a href="https://caniuse.com/#search=stream%20api" target="_blank">
-            <img src="<?php echo ROOT ?>img/stream-api.png" class="figure-img img-fluid rounded" alt="Stream API browser support">
+            <img src="<?php echo ROOT ?>img/stream-api.png" class="figure-img img-fluid rounded" alt="suportul pentru Stream API">
           </a>
-          <figcaption class="figure-caption">Stream API browser support</figcaption>
+          <figcaption class="figure-caption">suportul pentru Stream API</figcaption>
         </figure>
 
-        <h4>Getting Media Access</h4>
+        <h4>Obtinerea accesului la media (camera/microfon)</h4>
         <pre><code class="language-javascript">
 navigator.mediaDevices.getUserMedia(constraints)
 .then(function(stream) {
-  /* use the stream */
+  /* aici avem acces */
 })
 .catch(function(err) {
-  /* handle the error */
+  /* aici avem eroare */
 });
         </code></pre>
 
-        <h4>Applying constraints</h4>
-        <p>While information about a user's cameras and microphones are inaccessible for privacy reasons, an application can request the camera and microphone capabilities it needs and wants, using additional constraints.</p>
+        <h4>Aplicarea de constrangeri</h4>
+        <p>O aplicatie poate cere ca microfonul sau camera sa functioneze sub urmatoarele constrangeri</p>
         <pre><code class="language-javascript">
 {
-  audio: true,
+  audio: true, // or false
   video: { width: 1280, height: 720 }
 }
         </code></pre>
 
-        <p>As an API that may involve significant privacy concerns, getUserMedia() must always get user permission before opening any media gathering input such as a webcam or microphone. </p>
+        <p>Deoarece acest API implica nelinisti privind intimitatea utilizatorilor, functia getUserMedia() intotdeauna va functiona doar dupa ce obtine de la utilizator permisiunea folosirii camerei/microfonului. </p>
 
-        <h4>Using the new API in older browsers</h4>
+        <h4>Cum folosim API-ul Stream in browsere vechi</h4>
         <pre><code class="language-javascript">
-// Older browsers might not implement mediaDevices at all, so we set an empty object first
+// Majoritatea browserelor vechi nu au implementat obiectul mediaDevices 
+// Asa ca o initializam cu un obiect gol
 if (navigator.mediaDevices === undefined) {
   navigator.mediaDevices = {};
 }
 
-// Some browsers partially implement mediaDevices. We can't just assign an object
-// with getUserMedia as it would overwrite existing properties.
-// Here, we will just add the getUserMedia property if it's missing.
+// Unele browsere implementeaza partial obiectul mediaDevices. 
+// Asa ca vom adauga functia getUserMedia daca aceasta lipseste
 if (navigator.mediaDevices.getUserMedia === undefined) {
   navigator.mediaDevices.getUserMedia = function(constraints) {
 
-    // First get ahold of the legacy getUserMedia, if present
+    // Mai intai vom initializa functia getUserMedia
+    // cu una dintre valorile care pot exista deja in browserul curent
     var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-    // Some browsers just don't implement it - return a rejected promise with an error
-    // to keep a consistent interface
+    // Daca nicio valoare nu a fost gasita, inseamna ca browserul curent
+    // Nu permite deloc accesul la media
     if (!getUserMedia) {
-      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+      return Promise.reject(new Error('getUserMedia nu este implementat in acest browser'));
     }
 
-    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+    // Altfel, transmitem functia existenta catre navigator.getUserMedia 
+    // printr-o functie de tip promisiune
     return new Promise(function(resolve, reject) {
       getUserMedia.call(navigator, constraints, resolve, reject);
     });
@@ -72,11 +72,10 @@ if (navigator.mediaDevices.getUserMedia === undefined) {
 navigator.mediaDevices.getUserMedia({ audio: true, video: true })
 .then(function(stream) {
   var video = document.querySelector('video');
-  // Older browsers may not have srcObject
+  // Unele browsere vechi nu au implementat srcObject
   if ("srcObject" in video) {
     video.srcObject = stream;
   } else {
-    // Avoid using this in new browsers, as it is going away.
     video.src = window.URL.createObjectURL(stream);
   }
   video.onloadedmetadata = function(e) {
@@ -90,46 +89,47 @@ navigator.mediaDevices.getUserMedia({ audio: true, video: true })
 
         <br>
 
-        <h3>b) The Geolocation API</h3>
+        <h3>b) API-ul Geolocation</h3>
 
-        <p>The Geolocation API allows the user to provide their location to web applications if they so desire. For privacy reasons, the user is asked for permission to report location information.</p>
+        <p>Prin acest API putem obtine acces la GPS-ul dispozitivului. Din motive de securitate, utilizatorului i se cere mai intai permisiunea.</p>
 
-        <p>Browser support for Geolocation API can be checked here: <a href="https://caniuse.com/#search=Geolocation" target="_blank">Geolocation API browser support</a></p>
+        <p>Suportul browserelor pentru API-ul Geolocation poate fi verificat aici: <a href="https://caniuse.com/#search=Geolocation" target="_blank">Suport Geolocation API</a></p>
 
         <figure class="figure">
           <a href="https://caniuse.com/#search=Geolocation" target="_blank">
-            <img src="<?php echo ROOT ?>img/geolocation-api.png" class="figure-img img-fluid rounded" alt="Geolocation API browser support">
+            <img src="<?php echo ROOT ?>img/geolocation-api.png" class="figure-img img-fluid rounded" alt="Suport Geolocation API">
           </a>
-          <figcaption class="figure-caption">Geolocation API browser support</figcaption>
+          <figcaption class="figure-caption">Suport Geolocation API</figcaption>
         </figure>
 
-        <h4>The geolocation object</h4>
-        <p>If the object exists, geolocation services are available. You can test for the presence of geolocation like this:</p>
+        <h4>Obiectul geolocation</h4>
+        <p>Daca acest obiect existsa, serviciu geolocation este disponibil:</p>
         <pre><code class="language-javascript">
 if ("geolocation" in navigator) {
-  /* geolocation is available */
+  /* geolocation este dispobinil */
 } else {
-  /* geolocation IS NOT available */
+  /* geolocation NU este dispobinil */
 }
         </code></pre>
 
-        <h4>Getting the current position</h4>
-        <p>The getCurrentPosition() method. This initiates an asynchronous request to detect the user's position, and queries the positioning hardware to get up-to-date information.</p>
+        <h4>Obtinerea locatiei curente</h4>
+        <p>Functia getCurrentPosition() initieaza o cerere asincron pentru a detecta pozitia utilizatorului, si cere dispozitivului mobil sa obita informatia actualizata.</p>
         <pre><code class="language-javascript">
 navigator.geolocation.getCurrentPosition(function(position) {
+  // Aici avem acces la coordonatele latitudine si longitudine
   do_something(position.coords.latitude, position.coords.longitude);
 });
         </code></pre>
 
-        <h4>Watching the current position</h4>
-        <p>If the position data changes (either by device movement or if more accurate geo information arrives), you can set up a callback function that is called with that updated position information. </p>
+        <h4>Urmarirea locatiei curente</h4>
+        <p>Daca pozitia se schimba (datorita miscarii utilizatorului sau sunt obtinute informatii actualizate), se poate apela o functie in care se transmit informatiile actualizate. </p>
         <pre><code class="language-javascript">
 var watchID = navigator.geolocation.watchPosition(function(position) {
   do_something(position.coords.latitude, position.coords.longitude);
 });
         </code></pre>
 
-        <h4>Stop Watching the current position</h4>
+        <h4>Oprirea urmaririi locatiei curente</h4>
         <pre><code class="language-javascript">
 navigator.geolocation.clearWatch(watchID);
         </code></pre>
@@ -139,7 +139,7 @@ navigator.geolocation.clearWatch(watchID);
                 <li class="page-item backward">
                     <a class="page-link" href="<?php echo ROOT ?>push-notifications/" aria-label="Previous">
                         <span aria-hidden="true">&laquo;</span>
-                        <span class="paginationDesc">Push Notifications</span>
+                        <span class="paginationDesc">Notificari Push</span>
                     </a>
                 </li>
                 <li class="page-item forward">
